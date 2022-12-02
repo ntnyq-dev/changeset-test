93b1fe9936d3845d94c3b107d7cc70af70de5713
ntnyq <ntnyq13@gmail.com>
2022-12-02 12:14:08 +0800

    docs: init docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: `changesets-test`,
+
+  themeConfig: {
+
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# changesets-test
+
+hello world
